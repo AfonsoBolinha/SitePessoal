ce62c771a24a627de767383e3d196b8af1b388d2
Afonso Martins <afonsomartinspaivamiguel@gmail.com>
2023-06-14 20:39:00 +0100

    Add Project Goose

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 var gestorImg= ["Images/Gestor/welcome (1).png","Images/Gestor/Patrocinios.png","Images/Gestor/edit2.png","Images/Gestor/image_2022-12-24_190030831.png"];
 var caixaImg= ["Images/Caixa/Main.png","Images/Caixa/2.png","Images/Caixa/3.png","Images/Caixa/4.png"];
-
+var projectGoose=["Images/ProjectGoose/MainMenu.png","Images/ProjectGoose/Tuturial.png","Images/ProjectGoose/LVL2.png"]
 var timeout=2000;
-var play1=true,play2=true,play3=true;
+var play1=true,play2=true,play3=true,play4=true;
 async function showProjectInfo(projectId, img) {
     let projectInfo = document.getElementById(projectId);
     projectInfo.style.display = "block";
@@ -18,12 +18,23 @@ async function showProjectInfo(projectId, img) {
             }
         }
     }
-
     if (img == 3) {
         play3=true;
         let i = 0;
         while(i < gestorImg.length && play3) {
             gestor.src = gestorImg[i];
+            i++;
+            await new Promise(resolve => setTimeout(resolve, timeout));
+            if (i==3){
+                i=0;
+            }
+        }
+    }
+    if (img == 4) {
+        play4=true;
+        let i = 0;
+        while(i < projectGoose.length && play4) {
+            Goosegame.src = projectGoose[i];
             i++;
             await new Promise(resolve => setTimeout(resolve, timeout));
             if (i==3){
@@ -46,6 +57,10 @@ function hideProjectInfo(img) {
     }
     if (img==3){
         gestor.src=gestorImg[0];
+        play3=false;
+    }
+    if (img==4){
+        Goosegame.src=projectGoose[0];
         play3=false;
     }
 
